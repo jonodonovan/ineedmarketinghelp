cf44b268c0b30ed9d087b5d0a98a288608c28809
Jon <email@jodonovan.com>
2018-04-12 11:43:16 -0400

    Update to submit redirect

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -109,7 +109,7 @@ class JobController extends Controller
 
         // return redirect()->route('job.preview', [$job->record]);
         $job = Job::where('record', '=', $job->record)->firstOrFail();
-        return view('jobs.thankyou')->withJob($job);
+        return redirect()->route('jobs.thankyou')->withJob($job);
     }
 
     /**
